49ee716381102bb3728f87bcd4da5c72195e93a1
jimmyfresh1 <jamesfern14@gmail.com>
2023-12-11 01:14:04 -0600

    loop challenge

---
+++ Projects/JamesFernLoopChallenges/odds 1-20.js
@@ -0,0 +1,6 @@
+for (var i = 0 ; i <20 ; i++)
+{
+    if ((i+1)%2==0){
+        console.log(i)
+    }
+}
---
+++ Projects/JamesFernLoopChallenges/sequence.js
@@ -0,0 +1,7 @@
+
+function seqItems(arr) {
+    for (var i = 0 ; i < (arr.length) ; i++){
+        console.log(arr[i])
+        }
+    }
+    seqItems ([4, 2.5, 1, -.5, 2, -3.5])
