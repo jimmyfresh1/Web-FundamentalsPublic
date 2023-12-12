aee554ca45786bae00f381c1ad0a76831f1667ff
jimmyfresh1 <jamesfern14@gmail.com>
2023-12-12 12:54:01 -0600

    buttonclicker

--- Projects/JamesFernDojonary/script.js
+++ Projects/JamesFernDojonary/script.js
@@ -1,7 +1,20 @@
-function logIn() {
-    element.innerText = "Logout";
+function logOutLogIn(element){
+    if (element.innerText==='Login'){
+        element.innerText = "Logout";
+}
+    else if (element.innerText=== "Logout"){
+        element.innerText = "Login"
+    }
 }
 
-function smashThatLike () {
+function smashThatLike (element) {
+    alert("Please smash that like button!");
+    console.log(element.innerText)
+    var count=parseInt(element.innerText)
+    element.innerText = count + 1;
     
+}
+
+function hide(element) {
+    element.remove()
 }
